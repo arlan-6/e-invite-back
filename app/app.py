e1f825c37a1456a891f4c1312b8e8d872ee283ce
arlan-6 <arlanhan1997@gmail.com>
2024-11-24 19:35:11 +0500

    added link https://keliniz-kz.vercel.app/
    Signed-off-by: arlan-6 <arlanhan1997@gmail.com>

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ def create_app():
     app.config['DEBUG'] = config.DEBUG
     mongo.init_app(app)
 
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://production_url"]}})
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://keliniz-kz.vercel.app"]}})
 
     from app.routes.invite_routes import invite_bp
     app.register_blueprint(invite_bp)
